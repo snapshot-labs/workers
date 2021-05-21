73311b1e7bf8764e63add104c25b0fe8369bf726
Chaitanya <yourchaitu@gmail.com>
2021-05-21 15:31:07 +0530

    Add cache to mirror

--- mirror.ts
+++ mirror.ts
@@ -1,8 +1,17 @@
 addEventListener("fetch", event => {
-	event.respondWith(handleRequest(event.request))
+	event.respondWith(handleRequest(event))
 })
 
-async function handleRequest(request) {
+async function sha256(message) {
+	const msgBuffer = new TextEncoder().encode(message)
+	const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer)
+	const hashArray = Array.from(new Uint8Array(hashBuffer))
+	const hashHex = hashArray.map(b => ("00" + b.toString(16)).slice(-2)).join("")
+	return hashHex
+}
+
+async function handleRequest(event) {
+	const request = event.request;
 	let url = new URL(request.url)
 
 	let options = { cf: { image: {} } }
@@ -12,10 +21,23 @@ async function handleRequest(request) {
 	options.cf.image.height = 400
 
 	const imageURL = url.searchParams.get("img")
-
+	
+	const cache = caches.default
+	const hash = await sha256(imageURL)
+	const cacheUrl = new URL(request.url)
+	cacheUrl.pathname = "/snapshot" + cacheUrl.pathname + hash
+	cacheKey = new Request(cacheUrl.toString(), {
+		headers: request.headers,
+		method: "GET",
+	})
+	let response = await cache.match(cacheKey)
 	const imageRequest = new Request(imageURL, {
 		headers: request.headers,
 	})
 
-	return fetch(imageRequest, options)
+	if (!response) {
+		response = await fetch(imageRequest, options)
+		event.waitUntil(cache.put(cacheKey, response.clone()))
+	}
+	return response
 }
