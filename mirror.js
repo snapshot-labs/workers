711800591e37c312788078cc9f9cd97d4c313366
bigint <bigint@icloud.com>
2021-12-06 17:17:09 +0530

    Update mirror.js

--- mirror.js
+++ mirror.js
@@ -8,8 +8,8 @@ async function handleRequest(event, request) {
   let options = { cf: { image: {} } };
   options.cf.image.fit = 'cover';
   options.cf.image.anim = false;
-  options.cf.image.width = 400;
-  options.cf.image.height = 400;
+  options.cf.image.width = 240;
+  options.cf.image.height = 240;
 
   const imageURL = url.searchParams.get('img');
   if (!imageURL) return new Response('Missing "img" value', { status: 400 })
@@ -19,19 +19,24 @@ async function handleRequest(event, request) {
   const cacheKey = new Request(cacheUrl.toString(), request);
 
   let response = await cache.match(cacheKey);
-  const imageRequest = new Request(imageURL, {
-    headers: request.headers
-  });
-
-  if (!response) {
+  const hasCache = response ? true : false;
+  
+  if (!hasCache) {
+    const imageRequest = new Request(imageURL, { headers: request.headers });
     response = await fetch(imageRequest, options);
-    const type = response.headers.get("content-type");
-    if (type.includes('image/svg+xml') || type.includes('text/html')) {
-      response = new Response('Disallowed file extension', { status: 400 })
-    } else {
-      response = new Response(response.body, response);
-    }
-    response.headers.append('Cache-Control', 's-maxage=604800');
+  }
+
+  const type = response.headers.get("content-type");
+  console.log(type)
+  if (type.includes('image/svg+xml') || type.includes('text/html')) {
+    response = new Response('Disallowed file extension', { status: 400 })
+  }
+
+  response = new Response(response.body, response);
+  response.headers.append('Cache-Control', 's-maxage=60');
+  response.headers.append('Cache-Control', 'max-age=60');
+
+  if (!hasCache) {
     event.waitUntil(cache.put(cacheKey, response.clone()));
   }
 
